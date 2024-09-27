a7c08f91b926ff4aed793e16d82c3cb9b330634e
Gardo32 <gardodaqqaq@gmail.com>
2024-09-27 10:01:51 +0300

    Update Education.tsx

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -32,6 +32,27 @@ export default function Education() {
 							/>
 						</Fade>
 					</div>
+					<div className="group m-4 flex justify-between rounded-xl border-2 border-cyan-200 p-5 text-gray-700 hover:border-cyan-500 dark:border-gray-800  dark:text-white dark:hover:border-cyan-700 sm:col-span-2">
+						<Fade left>
+							<div>
+								<p className="mb-2 text-base text-cyan-600 dark:text-cyan-500">2022-2025</p>
+								<p className="my-1 font-medium sm:text-xl">Pearson BTEC International Level 3 Diploma in Information Technology</p>
+								<p className="my-1 text-sm font-medium">Training Body: Nasser Vocational Training Center  /  Awarding Body: Pearson BTEC </p>
+								<p className="text-sm">CGPA: P (Pass)</p>
+							</div>
+							<Image
+								src="pearson.png"
+								alt="Pearson Logo"
+								width={100}
+								height={100}
+								title="Pearson"
+								onClick={() => {
+									window.open('https://qualifications.pearson.com/content/dam/pdf/btec-international-level-3/it/specification-and-sample-assessments/btec-international-level-3-it-specification.pdf', '_blank');
+								}}
+								className="object-contain hover:cursor-pointer"
+							/>
+						</Fade>
+					</div>
 					{/* </Slide> */}
 				</div>
 			</div>
